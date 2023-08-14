38954aa2158b3f9eddb9f7165faa4e6a1ccaf766
Yunseon Shin <yunseon0518@naver.com>
2023-08-14 13:22:28 +0900

    한글 시작

---
+++ 보통.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+typedef string 끈;
+typedef void 공허;
+typedef int 정수;
+
+#define 중심 main
+#define 출력 cout
+#define 줄바꿈 endl
+#define 반환하다 return
+
+#define 영 0
+#define 하나 1
